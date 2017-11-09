85c84f5b03b93a47c76016b35ec3c6a6ba9333a9
nilin <11620967@qq.com>
2017-11-09 21:00:05 +0800

    Fix scan animation without paused.

--- app/src/main/java/com/nilin/etherealmuisc/activity/ScanMusicActivity.kt
+++ app/src/main/java/com/nilin/etherealmuisc/activity/ScanMusicActivity.kt
@@ -2,20 +2,18 @@ package com.nilin.etherealmuisc.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import com.nilin.etherealmuisc.Music
 import com.nilin.etherealmuisc.MyApplication
 import com.nilin.etherealmuisc.R
-import com.nilin.etherealmuisc.model.Song
-import com.nilin.etherealmuisc.utils.MusicUtils
 import com.nilin.etherealmuisc.utils.MusicUtils.getMusicData
 import kotlinx.android.synthetic.main.activity_scan_music.*
 import kotlinx.android.synthetic.main.include_app_bar.*
 import android.animation.ValueAnimator
 import android.view.animation.LinearInterpolator
 import android.animation.ObjectAnimator
-import com.nilin.etherealmuisc.view.ShaderView
+import android.widget.Toast
+import org.jetbrains.anko.custom.async
 
 
 /**
@@ -35,37 +33,37 @@ class ScanMusicActivity : AppCompatActivity(), View.OnClickListener {
 
     fun scanMusic() {
 
-        val degrees = ObjectAnimator.ofInt(sv, "degrees", 0, 360)
+        var degrees = ObjectAnimator.ofInt(sv, "degrees", 0, 360)
         degrees.interpolator = LinearInterpolator()
         degrees.duration = 1000
         degrees.repeatCount = ValueAnimator.INFINITE
         degrees.start()
-        /* new Thread(new Runnable() {
-          @Override
-          public void run() {
-            while (degrees <= 360) {
-              degrees += 1;
-              mShaderView.setDegrees(degrees);
 
-              try {
-                Thread.sleep(30);
-              } catch (InterruptedException e) {
-                e.printStackTrace();
-              }
+        async({
+            MyApplication.instance!!.getMusicDao().deleteAll()
+            for (i in 0..getMusicData(MyApplication.instance!!).size - 1) {
+                val list = Music(i.toLong(), getMusicData(MyApplication.instance!!).get(i).song, getMusicData(MyApplication.instance!!).get(i).singer, getMusicData(MyApplication.instance!!).get(i).path)
+                MyApplication.instance!!.getMusicDao().insertInTx(list)
             }
+            runOnUiThread {
+                degrees.cancel()
+                Toast.makeText(MyApplication.instance!!,"已扫描完毕",Toast.LENGTH_SHORT).show()
+            }
+        })
 
-
-          }
-        }).start();
-
-        degrees = 0;
-        mShaderView.setDegrees(degrees);*/
-
-        MyApplication.instance!!.getMusicDao().deleteAll()
-        for (i in 0..getMusicData(this).size - 1) {
-            val list = Music(i.toLong(), getMusicData(this).get(i).song, getMusicData(this).get(i).singer, getMusicData(this).get(i).path)
-            MyApplication.instance!!.getMusicDao().insertInTx(list)
-        }
+//        Thread(Runnable {
+//            var degrees = 0
+//            while (degrees <= 360) {
+//                degrees += 1
+//                sv.setDegrees(degrees)
+//                try {
+//                    Thread.sleep(10)
+//                } catch (e: InterruptedException) {
+//                    e.printStackTrace()
+//                }
+//            }
+//        }).start()
+        
     }
 
     override fun onClick(p0: View?) {
